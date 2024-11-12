b62263ad7de5e110acbdb4e3c70e994e2cf146ce
Hexcy <honeykyte@gmail.com>
2024-11-12 16:20:32 +0800

    feat:add calendar

--- src/components/dashboard/CalendarContent.vue
+++ src/components/dashboard/CalendarContent.vue
@@ -1,14 +1,31 @@
 <template>
     <v-container class="text-white">
-      <h2>Calendar Content</h2>
-      <p>This is the location content of the dashboard.</p>
+      <v-card class="units-cards text-white" height="auto" style="background-color:#2a2e3b; border-radius: 30px; padding: 20px;"> 
+        <FullCalendar :options="calendarOptions" />
+      </v-card>
     </v-container>
   </template>
   
   <script>
-  export default {
-    name: "CalendarContent",
-  };
+
+    import FullCalendar from '@fullcalendar/vue3'
+    import dayGridPlugin from '@fullcalendar/daygrid'
+    import interactionPlugin from '@fullcalendar/interaction'
+
+    export default {
+      components: {
+        FullCalendar // make the <FullCalendar> tag available
+      },
+      data() {
+        return {
+          calendarOptions: {
+            plugins: [ dayGridPlugin, interactionPlugin ],
+            initialView: 'dayGridMonth'
+          }
+        }
+      }
+    }
+
   </script>
   
   <style scoped>
